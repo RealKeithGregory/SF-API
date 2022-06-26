ab9712e55b1f47e7d429b8f246f473f6778e4403
RealKeithGregory <KMGIIGITHUB@GMAIL.COM>
2022-06-26 00:24:18 -0400

    created character object with character details

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const app = express()
 const PORT = 8000
+const character = {
+    'Ryu':{
+        'name': 'Ryu',
+        'age': 57,
+        'birthPlace': 'Japan',
+        'specialMove': 'Hadouken'
+    },
+    'Sagat':{
+        'name': 'Sagat',
+        'age': 58,
+        'birthPlace': 'Thailand',
+        'specialMove': 'Tiger Knee'
+    },
+    'M.Bison':{
+        'name': 'M.Bison',
+        'age': 'Unknown',
+        'birthPlace': 'Unknown',
+        'specialMove': 'Psycho Crusher' 
+    }
+
+}
 
 
 app.get('/',(request,response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
+app.get('/characters',(request, response) => {
+    response.json(character)
+})
+
 app.listen(PORT, () => {
-    console.log(`The server is running on ${PORT}!`)
+    console.log(`The server is running on port ${PORT}!`)
 })
