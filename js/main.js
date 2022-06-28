bf01b753db6994eac14e51d88bcf577f89654919
RealKeithGregory <KMGIIGITHUB@GMAIL.COM>
2022-06-27 21:50:05 -0400

    added client-side js to pull fighter name from api

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-alert('working')
-// document.addEventListener(onclick, grabName)
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const characterName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://kg-streetfighter-api.herokuapp.com/characters/${characterName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.name
+    }catch(error){
+        console.log(error)
+    }
+}
